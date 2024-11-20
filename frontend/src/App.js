e1f128f06ca3ed431976da4093187a0373332505
Sameh <sameh.edward@gmx.at>
2024-11-20 04:24:59 +0100

    header und Products List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
+import { Container } from "react-bootstrap";
 
-
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To CardShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
